942e1b0f8d1bca1b01169dd3d42f8e4e575af41a
vsRushy <gerardmarcosfreixasgmail.com>
2018-09-22 22:57:30 +0200

    TODO 2

--- Save&Load-Handout2/Motor2D/j1Scene.cpp
+++ Save&Load-Handout2/Motor2D/j1Scene.cpp
@@ -44,6 +44,11 @@ bool j1Scene::PreUpdate()
 bool j1Scene::Update(float dt)
 {
 	// TODO 2: Call load / save methods when pressing l/s
+	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
+		App->Save();
+	if (App->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
+		App->Load();
+
 
 	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
 		App->render->camera.y -= 1;
