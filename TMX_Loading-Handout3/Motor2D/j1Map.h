b5501fa7b7b851dd676e647c64dc39bb040f538e
GerardMF21 <32986524+vsRushy@users.noreply.github.com>
2018-09-26 12:01:25 +0200

    Handout 3 files. TODO 1, 2 done, TODO 3 in process
    Needs a little fix on TODO 3!!!

---
+++ TMX_Loading-Handout3/Motor2D/j1Map.h
@@ -0,0 +1,90 @@
+#ifndef __j1MAP_H__
+#define __j1MAP_H__
+
+#include "PugiXml/src/pugixml.hpp"
+#include "p2List.h"
+#include "p2Point.h"
+#include "j1Module.h"
+
+// TODO 2: Create a struct to hold information for a TileSet
+// Ignore Terrain Types and Tile Types for now, but we want the image!
+// ----------------------------------------------------
+struct Tileset
+{
+	uint first_grid = 0;
+	p2SString name = "";
+	uint tile_width = 0;
+	uint tile_height = 0;
+	uint spacing = 0;
+	uint margin = 0;
+};
+
+// TODO 1: Create a struct needed to hold the information to Map node
+enum class ORIENTATION
+{
+	ERROR_ = -1,
+	ORTHOGONAL,
+	ISOMETRIC,
+	ISOMETRIC_STEPPED,
+	HEXAGONAL_STEPPED
+};
+
+enum class RENDER_ORDER
+{
+	ERROR_ = -1,
+	RIGHT_DOWN,
+	RIGHT_UP,
+	LEFT_DOWN,
+	LEFT_UP
+};
+
+struct Map_info
+{
+	ORIENTATION orientation = ORIENTATION::ERROR_;
+	RENDER_ORDER render_order = RENDER_ORDER::ERROR_;
+	uint width = 0;
+	uint height = 0;
+	uint tile_width = 0;
+	uint tile_height = 0;
+	uint next_object_id = 0;
+};
+
+// ----------------------------------------------------
+class j1Map : public j1Module
+{
+public:
+
+	j1Map();
+
+	// Destructor
+	virtual ~j1Map();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node& conf);
+
+	// Called each loop iteration
+	void Draw();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Load new map
+	bool Load(const char* path);
+
+private:
+	// TODO 3
+	bool LoadMap(pugi::xml_document&);
+
+public:
+
+	// TODO 1: Add your struct for map info as public for now
+	Map_info map_info;
+
+private:
+
+	pugi::xml_document	map_file;
+	p2SString			folder;
+	bool				map_loaded;
+};
+
+#endif // __j1MAP_H__
