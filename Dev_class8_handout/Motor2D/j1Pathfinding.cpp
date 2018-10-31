36f36643aa265e01020f84bf148449777b67055a
GerardMF21 <32986524+vsRushy@users.noreply.github.com>
2018-10-31 11:23:16 +0100

    Until TODO 3 (idk if it's correct btw)

--- Dev_class8_handout/Motor2D/j1Pathfinding.cpp
+++ Dev_class8_handout/Motor2D/j1Pathfinding.cpp
@@ -168,16 +168,34 @@ int PathNode::CalculateF(const iPoint& destination)
 int j1PathFinding::CreatePath(const iPoint& origin, const iPoint& destination)
 {
 	// TODO 1: if origin or destination are not walkable, return -1
+	if (!IsWalkable(origin) || !IsWalkable(destination))
+	{
+		return -1;
+	}
 
 	// TODO 2: Create two lists: open, close
 	// Add the origin tile to open
 	// Iterate while we have tile in the open list
+	PathList open;
+	PathList close;
+
+	open.list.start->data.pos = origin;
+	p2List_item<PathNode>* open_item = open.list.start;
+	while (open_item != nullptr)
+	{
+		open.list.add(open_item->data);
+		open_item = open_item->next;
+	}
 
 	// TODO 3: Move the lowest score cell from open list to the closed list
-	
+	open_item = open.GetNodeLowestScore();
+	close.list.add(open_item->data);
+	open.list.del(open_item);
+
 	// TODO 4: If we just added the destination, we are done!
 	// Backtrack to create the final path
 	// Use the Pathnode::parent and Flip() the path when you are finish
+
 
 	// TODO 5: Fill a list of all adjancent nodes
 
